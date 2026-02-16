ff2a2c4ba61a48d2ae5ebbbc129f497e3ed68f4f
dreamlaserinnovations-prog <dreamlaserinnovations@gmail.com>
2026-02-16 15:45:18 -0500

    fix elenty date filter + build

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,26 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
+
+  // Date filter
+  eleventyConfig.addFilter("date", (dateObj, format = "MMM dd, yyyy", zone = "utc") => {
+    if (!dateObj) return "";
+
+    const dt =
+      dateObj instanceof Date
+        ? DateTime.fromJSDate(dateObj, { zone })
+        : DateTime.fromISO(String(dateObj), { zone });
+
+    return dt.isValid ? dt.toFormat(format) : "";
+  });
+
   // Static assets
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("css");
 
   // Blog collection
-  eleventyConfig.addCollection("posts", function(collectionApi) {
+  eleventyConfig.addCollection("posts", function (collectionApi) {
     return collectionApi.getFilteredByGlob("posts/*.md");
   });
 
